323d1d79e2684c6cee986e229e0f7fe07f1fb401
alisahibmammadov <ali.yek.10@mail.ru>
2024-01-13 21:44:07 +0400

    MongoDB downloaded

---
+++ mongo.js
@@ -0,0 +1,8 @@
+const MongoClient = require("mongodb").MongoClient;
+const url =
+  "mongodb+srv://alisahib:alisahib@cluster0.hpksjxu.mongodb.net/?retryWrites=true&w=majority";
+const createProduct = async (req, res, next) => {};
+const getProducts = async (req, res, next) => {};
+
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
