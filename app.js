98fa7fa9c0bce81108feeca1558579d7a7703840
alisahibmammadov <ali.yek.10@mail.ru>
2024-01-13 22:47:59 +0400

    DB Connection and post  data && get data

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mondoPratice = require("./mongo");
 const app = express();
 
 app.use(bodyParser.json());
-app.post("/products");
-app.get("/products");
+app.post("/products", mondoPratice.createProduct);
+app.get("/products", mondoPratice.getProducts);
 
-app.listen(3000);
+app.listen(4000);
